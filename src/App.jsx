9c7d0ad7a14e68d3c42e4ad23efe26db7711bac2
Sumit dhakal <sumitdhakal082@gmail.com>
2026-03-07 20:18:11 +0545

    minor changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import "./App.css";
 import About from "./components/About";
+import Contact from "./components/Contact";
 import Footer from "./components/Footer";
 import Hero from "./components/Hero";
 import NavBar from "./components/NavBar";
@@ -14,6 +15,7 @@ function App() {
       <About />
       <Skills />
       <Project />
+      <Contact />
       <Footer />
     </div>
   );
